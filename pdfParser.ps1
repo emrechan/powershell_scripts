671cdd1919913cb9a5c4a0ff0a7ec4f7615d9f76
Emrecan Arda <emrechan@gmail.com>
2017-05-21 12:31:14 +0300

    pdfParser is a tool to parse service packs pdfs

---
+++ pdfParser.ps1
@@ -0,0 +1,49 @@
+﻿param (
+    [string]$file
+)
+Add-Type -Path .\itextsharp.dll
+$cwd = Get-Location
+
+$pdf = $cwd.Path + "\1058DS0837_SP90_1.0.1_Rev-.pdf"
+
+$reader = New-Object iTextSharp.text.pdf.pdfreader -ArgumentList $file
+
+
+for ($page = 1; $page -le $reader.NumberOfPages; $page++) {
+    $strategy = new-object  'iTextSharp.text.pdf.parser.SimpleTextExtractionStrategy'
+    $currentText = [iTextSharp.text.pdf.parser.PdfTextExtractor]::GetTextFromPage($reader, $page, $strategy);
+    #[string[]]$Text += [system.text.Encoding]::UTF8.GetString([System.Text.ASCIIEncoding]::Convert( [system.text.encoding]::default  , [system.text.encoding]::UTF8, [system.text.Encoding]::Default.GetBytes($currentText)));
+    [string[]]$currentLine = [system.text.Encoding]::UTF8.GetString([System.Text.ASCIIEncoding]::Convert( [system.text.encoding]::default  , [system.text.encoding]::UTF8, [system.text.Encoding]::Default.GetBytes($currentText)));
+    foreach ($line in $currentLine.Split([char]0x000A)) {
+        if ($line -like "*which are obsolete*") {
+            $startIndex = $line.IndexOf(":")
+            $startIndex += 1
+            $wr = $prevLine + ";" + $line.Substring($startIndex)
+            $add = True
+        } ElseIf ($line -like "*Installation Nodes*") {
+            $startIndex = $line.IndexOf(":")
+            $startIndex += 1
+            $wr += ";" + $line.Substring($startIndex)
+        } ElseIf ($line -like "*Require ACCS System Stop*") {
+            $startIndex = $line.IndexOf(":")
+            $startIndex += 1
+            $wr += ";" + $line.Substring($startIndex)
+        } ElseIf ($line -like "*Require Server reboot*") {
+            $startIndex = $line.IndexOf(":")
+            $startIndex += 1
+            $wr += ";" + $line.Substring($startIndex)
+        } ElseIf ($line -like "*Installation Duration*") {
+            $startIndex = $line.IndexOf(":")
+            $startIndex += 1
+            $wr += ";" + $line.Substring($startIndex)
+            Write-Output $wr
+        } Else {
+            $prevLine = $line    
+        }        
+        
+    }
+    #Write-Output $currentText
+}
+#Write-Output $Text
+
+$reader.Close()
---
+++ pdfParser_v1.ps1
@@ -0,0 +1,82 @@
+﻿param (
+    [string]$file
+)
+Add-Type -Path .\itextsharp.dll
+$cwd = Get-Location
+
+$pdf = $cwd.Path + "\DocDjango_SP92_1.0.0_28pkgs_27avril2017.pdf"
+
+$reader = New-Object iTextSharp.text.pdf.pdfreader -ArgumentList $file
+
+Write-Output "Item Name;Obsolete;Uninstall Prev;Install Dep;Runtime Dep;Install Nodes;Accs Stop;Server Reboot;Install Duration"
+for ($page = 1; $page -le $reader.NumberOfPages; $page++) {
+    $strategy = new-object  'iTextSharp.text.pdf.parser.SimpleTextExtractionStrategy'
+    $currentText = [iTextSharp.text.pdf.parser.PdfTextExtractor]::GetTextFromPage($reader, $page, $strategy);
+    #[string[]]$Text += [system.text.Encoding]::UTF8.GetString([System.Text.ASCIIEncoding]::Convert( [system.text.encoding]::default  , [system.text.encoding]::UTF8, [system.text.Encoding]::Default.GetBytes($currentText)));
+    [string[]]$currentLine = [system.text.Encoding]::UTF8.GetString([System.Text.ASCIIEncoding]::Convert( [system.text.encoding]::default  , [system.text.encoding]::UTF8, [system.text.Encoding]::Default.GetBytes($currentText)));
+    foreach ($line in $currentLine.Split([char]0x000A)) {
+        if ($line -like "*which are obsolete*") {
+            $wr = $line
+            $itemName = $prevLine
+            $add = 1
+        } ElseIf ($line -like "*Comment*") {
+            $startIndex = $wr.IndexOf(":")
+            $startIndex += 1
+            $obsolete = $wr.Substring($startIndex)
+            $add = 0
+        } ElseIf ($line -like "*Uninstall previous Version*") {
+            $startIndex = $line.IndexOf(":")
+            $startIndex += 1
+            $uninstallPrev= $line.Substring($startIndex)
+        } ElseIf ($line -like "*Installation Dependencies*") {
+            $wr = $line
+            $add = 1
+        } ElseIf ($line -like "*Runtime Dependencies*") {
+            $startIndex = $wr.IndexOf(":")
+            $startIndex += 1
+            $installDep = $wr.Substring($startIndex)
+            $wr = $line
+        } ElseIf ($line -like "*PTR(s)*") {
+            $startIndex = $wr.IndexOf(":")
+            $startIndex += 1
+            $runtimeDep = $wr.Substring($startIndex)
+            $add = 0
+        } ElseIf ($line -like "*Installation Nodes*") {
+            $startIndex = $line.IndexOf(":")
+            $startIndex += 1
+            $installNodes= $line.Substring($startIndex)
+        } ElseIf ($line -like "*Require ACCS System Stop*") {
+            $startIndex = $line.IndexOf(":")
+            $startIndex += 1
+            $accsStop= $line.Substring($startIndex)
+        } ElseIf ($line -like "*Require Server reboot*") {
+            $startIndex = $line.IndexOf(":")
+            $startIndex += 1
+            $serverReboot= $line.Substring($startIndex)
+        } ElseIf ($line -like "*Installation Duration*") {
+            $startIndex = $line.IndexOf(":")
+            $startIndex += 1
+            $installDuration= $line.Substring($startIndex)
+            $wr = $itemName + ";" + $obsolete + ";" + $uninstallPrev + ";" + $installDep + ";" + $runtimeDep + ";" + $installNodes + ";" + $accsStop + ";" + $serverReboot + ";" + $installDuration
+            Write-Output $wr
+            $itemName = ""
+            $obsolete = ""
+            $uninstallPrev = ""
+            $installDep = ""
+            $runtimeDep = ""
+            $installNodes = ""
+            $accsStop = ""
+            $serverReboot = "" 
+            $installDuration = ""
+        } ElseIf($add) {
+            $wr += $line
+        } Else {
+            $prevLine = $line    
+        }        
+        
+    }
+    #Write-Output $currentText
+}
+#Write-Output $Text
+
+$reader.Close()
